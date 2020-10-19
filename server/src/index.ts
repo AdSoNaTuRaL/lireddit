a8e9d3125c89d76ff474d16baaf59f0efe261129
adsonatural <adsonhenriquesilva@gmail.com>
2020-10-19 17:59:08 +0200

    Adding cors to use on express

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
-import { MyContext } from "./types";
+import cors from "cors";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
@@ -20,7 +20,11 @@ const main = async () => {
     const app = express();
 
     const RedisStore = connectRedis(session);
-    const redisClient = redis.createClient()
+    const redisClient = redis.createClient();
+    app.use(cors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    }));
 
     app.use(
         session({
@@ -46,10 +50,13 @@ const main = async () => {
             resolvers: [HelloResolver, PostResolver, UserResolver],
             validate: false
         }),
-        context: ({ req, res }): MyContext => ({ em: orm.em, req, res })
+        context: ({ req, res }) => ({ em: orm.em, req, res })
     });
 
-    apolloServer.applyMiddleware({ app });
+    apolloServer.applyMiddleware({ 
+      app, 
+      cors: false, 
+    });
     
     app.listen(4000, () => {
         console.log('🚀 Server started on localhost:4000')
