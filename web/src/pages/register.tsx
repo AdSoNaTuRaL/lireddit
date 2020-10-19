779450db41976184ba7f2dda892b7bfbff801ade
adsonatural <adsonhenriquesilva@gmail.com>
2020-10-19 18:02:33 +0200

    Create action for register user

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,20 +1,31 @@
-import React from 'react'
+import React from 'react';
 import { Formik, Form } from 'formik';
 import { Box, Button} from '@chakra-ui/core';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
+import { useRegisterMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
+import { useRouter } from 'next/router';
 
-interface registerProps {
-
-}
+interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
+  const router = useRouter();
+  const [, register] = useRegisterMutation();
+
   return (
     <Wrapper variant='small'>
       <Formik 
         initialValues={{username: '', password: ''}}
-        onSubmit={(values) => {
-          console.log(values);
+        onSubmit={async (values, {setErrors}) => {
+          const response = await register(values);
+          console.log(response);
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors))
+          } else if (response.data?.register.user) {
+            // worked
+            router.push("/");
+          }
         }}
       >
         {({isSubmitting}) => (
