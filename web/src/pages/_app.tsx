62c4616afd3aeb8f4ffad6fceadb0c6f9e6256ee
adsonatural <adsonhenriquesilva@gmail.com>
2020-10-19 18:02:17 +0200

    refactoring _app.tsx

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,16 +1,26 @@
-import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core'
+import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core';
+import { Provider, createClient } from 'urql';
+
+const client = createClient({ 
+  url: 'http://localhost:4000/graphql',
+  fetchOptions: {
+    credentials: 'include',
+  },
+});
 
 import theme from '../theme'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: any) {
   return (
-    <ThemeProvider theme={theme}>
-      <ColorModeProvider>
-        <CSSReset />
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ThemeProvider>
-  )
+    <Provider value={client}>
+      <ThemeProvider theme={theme}>
+        <ColorModeProvider>
+          <CSSReset />
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ThemeProvider>
+    </Provider>
+  );
 }
 
 export default MyApp
