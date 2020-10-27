97f5205e69d3d6f34c67cad04e4c87c395457794
adsonatural <adsonhenriquesilva@gmail.com>
2020-10-27 11:13:12 +0100

    Updating pages

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,3 +1,10 @@
-const Index = () => <div>Hello World</div>
+import { NavBar } from "../components/NavBar";
+
+const Index = () => (
+  <>
+    <NavBar />
+    <div>Hello world</div>
+  </>
+)
 
 export default Index;
