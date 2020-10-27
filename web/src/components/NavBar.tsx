b087f2da69514911c253e6f264ea310018f26316
adsonatural <adsonhenriquesilva@gmail.com>
2020-10-27 12:46:54 +0100

    refactoring and adding new things

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,14 +1,18 @@
 import { Box, Button, Flex, Link } from '@chakra-ui/core';
 import NextLink from 'next/link';
 import React from 'react';
-import { useMeQuery } from '../generated/graphql';
+import { useLogoutMutation, useMeQuery } from '../generated/graphql';
+import { isServer } from '../utils/isServer';
 
 interface NavBarProps {
 
 }
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
-  const [{data, fetching}] = useMeQuery();
+  const [{fetching: logoutFetching}, logout] = useLogoutMutation();
+  const [{data, fetching}] = useMeQuery({
+    pause: isServer(),
+  });
   let body = null;
 
   //data is loading
@@ -33,7 +37,15 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
     body = (
       <Flex>
         <Box mr={2}>{data.me.username}</Box>
-        <Button variant='link'>logout</Button>
+        <Button 
+          onClick={() => {
+            logout();
+          }}
+          isLoading={logoutFetching}
+          variant='link'
+        >
+          logout
+        </Button>
       </Flex>
     );
   }
