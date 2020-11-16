128f6b8aac0a54371f10ae0250a3696f1169fa59
adsonatural <adsonhenriquesilva@gmail.com>
2020-11-16 12:02:05 +0100

    Adding relations on entities

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -1,5 +1,13 @@
 import { ObjectType, Field } from "type-graphql";
-import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import {
+  BaseEntity, 
+  Column, 
+  CreateDateColumn, 
+  Entity, ManyToOne, 
+  PrimaryGeneratedColumn, 
+  UpdateDateColumn 
+} from "typeorm";
+import { User } from "./User";
 
 @ObjectType()
 @Entity()
@@ -7,6 +15,25 @@ export class Post extends BaseEntity {
   @Field()
   @PrimaryGeneratedColumn()
   id!: number;
+
+  @Field()
+  @Column()
+  title!: string;
+
+  @Field()
+  @Column()
+  text!: string;
+
+  @Field()
+  @Column({ type: "int", default: 0 })
+  points!: number;
+
+  @Field()
+  @Column()
+  creatorId: number;
+
+  @ManyToOne(() => User, (user) => user.posts)
+  creator: User;
 
   @Field(() => String)
   @CreateDateColumn()
@@ -16,7 +43,4 @@ export class Post extends BaseEntity {
   @UpdateDateColumn()
   updatedAt: Date;
 
-  @Field()
-  @Column()
-  title!: string;
 }
