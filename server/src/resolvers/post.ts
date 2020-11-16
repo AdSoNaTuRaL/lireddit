d11d71a652243f5c36e270cf7b9b4add65789f58
adsonatural <adsonhenriquesilva@gmail.com>
2020-11-16 13:00:03 +0100

    Adding pagination and limit for posts

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,7 +1,8 @@
 import { isAuth } from "../middleware/isAuth";
 import { MyContext } from "../types";
-import { Resolver, Query, Arg, Mutation, InputType, Field, Ctx, UseMiddleware } from "type-graphql";
+import { Resolver, Query, Arg, Mutation, InputType, Field, Ctx, UseMiddleware, Int } from "type-graphql";
 import { Post } from "../entities/Post";
+import { getConnection } from "typeorm";
 
 @InputType()
 class PostInput {
@@ -14,8 +15,22 @@ class PostInput {
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  async posts(): Promise<Post[]> {
-    return Post.find();
+  async posts(
+    @Arg('limit', () => Int) limit: number,
+    @Arg('cursor', () => String, { nullable: true }) cursor: string | null
+  ): Promise<Post[]> {
+    const realLimit = Math.min(50, limit);
+    const qb = getConnection()
+    .getRepository(Post)
+    .createQueryBuilder("p")
+    .orderBy('"createdAt"', 'DESC')
+    .take(realLimit)
+
+    if(cursor) {
+      qb.where('"createdAt" < :cursor', { cursor: new Date(parseInt(cursor)) })
+    }
+
+    return qb.getMany();
   }
 
   @Query(() => Post, { nullable: true })
