5722e4b569566a4087c835303078043f0b760f2a
adsonatural <adsonhenriquesilva@gmail.com>
2020-11-17 12:37:54 +0100

    Adding styles for each post

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,28 +1,52 @@
-import { Link } from "@chakra-ui/core";
+import { Box, Button, Flex, Heading, Link, Stack, Text } from "@chakra-ui/core";
 import { withUrqlClient } from "next-urql";
 import { Layout } from "../components/Layout";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import NextLink from "next/link";
+import { useState } from "react";
 
 const Index = () => {
-  const [{data}] = usePostsQuery({
-    variables: {
-      limit: 10,
-    },
+  const [variables, setVariables] = useState({limit: 10, cursor: null as null | string});
+  const [{ data, fetching }] = usePostsQuery({
+    variables,
   });
+
+  if (!fetching && !data) {
+    return <div>You got no posts for some reason</div>;
+  }
 
   return (
     <Layout>
-      <NextLink href="/create-post">
-        <Link>Create Post</Link>
-      </NextLink>
+      <Flex align="center">
+        <Heading>LiReddit</Heading>
+        <NextLink href="/create-post">
+          <Link ml='auto'>Create Post</Link>
+        </NextLink>
+      </Flex>
       <br />
-      {!data ? (
+      {!data && fetching ? (
         <div>loading...</div>
        ) : (
-         data.posts.map(p => <div key={p.id}>{p.title}</div>)
+        <Stack spacing={8}>
+          {data!.posts.posts.map((p) => (
+            <Box key={p.id} p={5} shadow="md" borderWidth="1px">
+            <Heading fontSize="xl">{p.title}</Heading>
+            <Text mt={4}>{p.textSnippet}</Text>
+            </Box>
+          ))}
+        </Stack>
        )}
+      {data && data.posts.hasMore ? ( 
+        <Flex>
+          <Button onClick={() => {
+            setVariables({
+              limit: variables.limit,
+              cursor: data.posts.posts[data.posts.posts.length -1].createdAt,
+            })
+          }} isLoading={fetching} m="auto" my={8}>Load more</Button>
+        </Flex>
+      ) : null}
     </Layout>
   );
 };
