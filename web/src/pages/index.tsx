52fd9269b1f79f1a5e8ac71d1c37af0faf1e6f6c
adsonatural <adsonhenriquesilva@gmail.com>
2020-11-18 13:45:00 +0100

    Adding style for votes on Post

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,13 +1,17 @@
-import { Box, Button, Flex, Heading, Link, Stack, Text } from "@chakra-ui/core";
+import { Box, Button, Flex, Heading, Icon, IconButton, Link, Stack, Text } from "@chakra-ui/core";
 import { withUrqlClient } from "next-urql";
 import { Layout } from "../components/Layout";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import NextLink from "next/link";
 import { useState } from "react";
+import { UpdootSection } from "../components/UpdootSection";
 
 const Index = () => {
-  const [variables, setVariables] = useState({limit: 10, cursor: null as null | string});
+  const [variables, setVariables] = useState({
+    limit: 15, 
+    cursor: null as null | string
+  });
   const [{ data, fetching }] = usePostsQuery({
     variables,
   });
@@ -30,10 +34,14 @@ const Index = () => {
        ) : (
         <Stack spacing={8}>
           {data!.posts.posts.map((p) => (
-            <Box key={p.id} p={5} shadow="md" borderWidth="1px">
-            <Heading fontSize="xl">{p.title}</Heading>
-            <Text mt={4}>{p.textSnippet}</Text>
-            </Box>
+            <Flex key={p.id} p={5} shadow="md" borderWidth="1px">
+              <UpdootSection post={p} />
+              <Box>
+                <Heading fontSize="xl">{p.title}</Heading>
+                <Text>Posted by: {p.creator.username}</Text>
+                <Text mt={4}>{p.textSnippet}</Text>
+              </Box>
+            </Flex>
           ))}
         </Stack>
        )}
