9713aa04468fb2d375b8ae57e99b19adbad13085
adsonatural <adsonhenriquesilva@gmail.com>
2020-11-18 13:43:37 +0100

    Adding new mutations

--- web/src/utils/createUrqlClient.ts
+++ web/src/utils/createUrqlClient.ts
@@ -1,15 +1,20 @@
-import { dedupExchange, Exchange, fetchExchange, stringifyVariables } from "urql";
+import { 
+  dedupExchange, Exchange, 
+  fetchExchange, stringifyVariables 
+} from "urql";
 import { cacheExchange, Resolver } from '@urql/exchange-graphcache';
 import {
   LogoutMutation,
   MeQuery,
   MeDocument,
   LoginMutation,
-  RegisterMutation
+  RegisterMutation,
+  VoteMutationVariables
 } from "../generated/graphql";
 import { betterUpdateQuery } from "./betterUpdateQuery";
 import { pipe, tap } from 'wonka';
 import Router from "next/router";
+import gql from "graphql-tag";
 
 const errorExchange: Exchange = ({ forward }) => ops$ => {
   return pipe(
@@ -78,6 +83,46 @@ export const createUrqlClient = (ssrExchange: any) => ({
       },
       updates: {
         Mutation: {
+          vote: (_result, args, cache, info) => {
+            const { postId, value } = args as VoteMutationVariables;
+            const data = cache.readFragment(
+              gql`
+                fragment _ on Post {
+                  id
+                  points
+                  voteStatus
+                }
+              `,
+              { id: postId } as any
+            );
+
+            if (data) {
+              if (data.voteStatus === value) {
+                return;
+              }
+              const newPoints = 
+                (data.points as number) + ((!data.voteStatus ? 1 : 2) * value);
+              cache.writeFragment(
+                gql`
+                  fragment __ on Post {
+                    points
+                    voteStatus
+                  }
+                `,
+                { id: postId, points: newPoints, voteStatus: value } as any
+              );
+            }
+          },
+          createPost: (_result, args, cache, info) => {
+            const allFields = cache.inspectFields('Query');
+            const fieldInfos = allFields.filter(
+              (info) => info.fieldName === 'posts'
+            );
+            fieldInfos.forEach((fi) => {
+              cache.invalidate('Query', 'posts', fi.arguments || {});
+            })
+            
+          },
           logout: (_result, args, cache, info) => {
             betterUpdateQuery<LogoutMutation, MeQuery>(
               cache,
