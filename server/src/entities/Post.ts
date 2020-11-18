f1ac173d0692f7a7b9f035c2ddd260db544497ee
adsonatural <adsonhenriquesilva@gmail.com>
2020-11-18 13:44:33 +0100

    Updating entities Post and User with new fields and relations with Updoot

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -1,12 +1,14 @@
-import { ObjectType, Field } from "type-graphql";
+import { ObjectType, Field, Int } from "type-graphql";
 import {
   BaseEntity, 
   Column, 
   CreateDateColumn, 
   Entity, ManyToOne, 
+  OneToMany, 
   PrimaryGeneratedColumn, 
   UpdateDateColumn 
 } from "typeorm";
+import { Updoot } from "./Updoot";
 import { User } from "./User";
 
 @ObjectType()
@@ -28,6 +30,9 @@ export class Post extends BaseEntity {
   @Column({ type: "int", default: 0 })
   points!: number;
 
+  @Field(() => Int, { nullable: true })
+  voteStatus: number | null;
+
   @Field()
   @Column()
   creatorId: number;
@@ -35,6 +40,9 @@ export class Post extends BaseEntity {
   @Field()
   @ManyToOne(() => User, (user) => user.posts)
   creator: User;
+
+  @OneToMany(() => Updoot, (updoot) => updoot.post)
+  updoots: Updoot[];
 
   @Field(() => String)
   @CreateDateColumn()
