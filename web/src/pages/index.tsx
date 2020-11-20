d288397d7e0c89b7dcd347842cf150e6c15a8264
adsonatural <adsonhenriquesilva@gmail.com>
2020-11-20 12:17:39 +0100

    Just getting error and showing that on case happens error

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -15,52 +15,57 @@ import { EditDeletePostButtons } from "../components/EditDeletePostButtons";
 
 const Index = () => {
   const [variables, setVariables] = useState({
-    limit: 15, 
+    limit: 15,
     cursor: null as null | string
   });
 
 
-  const [{ data, fetching }] = usePostsQuery({
+  const [{ data, error, fetching }] = usePostsQuery({
     variables,
   });
 
   if (!fetching && !data) {
-    return <div>You got no posts for some reason</div>;
+    return (
+      <div>
+        <div>You got no posts for some reason</div>
+        <div>{error?.message}</div>
+      </div>
+    );
   }
 
   return (
     <Layout>
       {!data && fetching ? (
         <div>loading...</div>
-       ) : (
-        <Stack spacing={8}>
-          {data!.posts.posts.map((p) => !p ? null : (
-            <Flex key={p.id} p={5} shadow="md" borderWidth="1px">
-              <UpdootSection post={p} />
-              <Box flex={1}>
-                <NextLink href="/post/[id]" as={`/post/${p.id}`}>
-                  <Link>
-                    <Heading fontSize="xl">{p.title}</Heading>
-                  </Link>
-                </NextLink>
-                <Text>Posted by: {p.creator.username}</Text>
-                <Flex align="center">
-                  <Text flex={1} mt={4}>{p.textSnippet}</Text>
-                  <Box ml="auto">
-                    <EditDeletePostButtons id={p.id} creatorId={p.creator.id} />
-                  </Box>
-                </Flex>
-              </Box>
-            </Flex>
-          ))}
-        </Stack>
-       )}
-      {data && data.posts.hasMore ? ( 
+      ) : (
+          <Stack spacing={8}>
+            {data!.posts.posts.map((p) => !p ? null : (
+              <Flex key={p.id} p={5} shadow="md" borderWidth="1px">
+                <UpdootSection post={p} />
+                <Box flex={1}>
+                  <NextLink href="/post/[id]" as={`/post/${p.id}`}>
+                    <Link>
+                      <Heading fontSize="xl">{p.title}</Heading>
+                    </Link>
+                  </NextLink>
+                  <Text>Posted by: {p.creator.username}</Text>
+                  <Flex align="center">
+                    <Text flex={1} mt={4}>{p.textSnippet}</Text>
+                    <Box ml="auto">
+                      <EditDeletePostButtons id={p.id} creatorId={p.creator.id} />
+                    </Box>
+                  </Flex>
+                </Box>
+              </Flex>
+            ))}
+          </Stack>
+        )}
+      {data && data.posts.hasMore ? (
         <Flex>
           <Button onClick={() => {
             setVariables({
               limit: variables.limit,
-              cursor: data.posts.posts[data.posts.posts.length -1].createdAt,
+              cursor: data.posts.posts[data.posts.posts.length - 1].createdAt,
             })
           }} isLoading={fetching} m="auto" my={8}>Load more</Button>
         </Flex>
