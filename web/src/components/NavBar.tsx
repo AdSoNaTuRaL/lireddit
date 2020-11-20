37707a96681ae14cf9b141ce5d33d2fa98291f8d
adsonatural <adsonhenriquesilva@gmail.com>
2020-11-20 12:15:14 +0100

    Invalidating cache on logout

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -3,10 +3,12 @@ import NextLink from 'next/link';
 import React from 'react';
 import { useLogoutMutation, useMeQuery } from '../generated/graphql';
 import { isServer } from '../utils/isServer';
+import { useRouter } from 'next/router';
 
 interface NavBarProps { }
 
 export const NavBar: React.FC<NavBarProps> = ({ }) => {
+  const router = useRouter();
   const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
   const [{ data, fetching }] = useMeQuery({
     pause: isServer(),
@@ -41,8 +43,9 @@ export const NavBar: React.FC<NavBarProps> = ({ }) => {
         </NextLink>
         <Box mr={2}>{data.me.username}</Box>
         <Button
-          onClick={() => {
-            logout();
+          onClick={async () => {
+            await logout();
+            router.reload();
           }}
           isLoading={logoutFetching}
           variant='link'
