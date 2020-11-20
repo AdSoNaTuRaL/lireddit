048b318bf68c0dfecf76c8851b1841f1956593d4
adsonatural <adsonhenriquesilva@gmail.com>
2020-11-20 12:16:18 +0100

    Using loaders on context

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,67 +15,75 @@ import { Post } from "./entities/Post";
 import { User } from "./entities/User";
 import path from "path";
 import { Updoot } from "./entities/Updoot";
+import { createUserLoader } from "./utils/createUserLoader";
+import { createUpdootLoader } from "./utils/createUpdootLoader";
 
 const main = async () => {
-    const conn = await createConnection({
-        type: 'postgres',
-        database: 'lireddit2',
-        username: 'postgres',
-        password: 'docker',
-        logging: true,
-        synchronize: true,
-        migrations: [path.join(__dirname, './migrations/*')],
-        entities: [Post, User, Updoot],
-    });
-    await conn.runMigrations();
+  const conn = await createConnection({
+    type: 'postgres',
+    database: 'lireddit2',
+    username: 'postgres',
+    password: 'docker',
+    logging: true,
+    synchronize: true,
+    migrations: [path.join(__dirname, './migrations/*')],
+    entities: [Post, User, Updoot],
+  });
+  await conn.runMigrations();
 
-    // await Post.delete({});
+  // await Post.delete({});
 
-    const app = express();
+  const app = express();
 
-    const RedisStore = connectRedis(session);
-    const redis = new Redis();
+  const RedisStore = connectRedis(session);
+  const redis = new Redis();
 
-    app.use(cors({
-      origin: 'http://localhost:3000',
-      credentials: true,
-    }));
+  app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }));
 
-    app.use(
-        session({
-            name: COOKIE_NAME,
-            store: new RedisStore({
-                client: redis,
-                disableTouch: true,
-            }),
-            cookie: {
-                maxAge: 1000 * 60 * 60 * 24 * 365 * 10, //10 years
-                httpOnly: true,
-                sameSite: 'lax', //csrf
-                secure: __prod__ //cookie only works in https
-            },
-            saveUninitialized: false,
-            secret: 'asdoifjasiodfjasodfijasodifj',
-            resave: false,
-        })
-    )
-
-    const apolloServer = new ApolloServer({
-        schema: await buildSchema({
-            resolvers: [HelloResolver, PostResolver, UserResolver],
-            validate: false
-        }),
-        context: ({ req, res }) => ({ req, res, redis })
-    });
-
-    apolloServer.applyMiddleware({ 
-      app, 
-      cors: false, 
-    });
-    
-    app.listen(4000, () => {
-        console.log('🚀 Server started on localhost:4000')
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      store: new RedisStore({
+        client: redis,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, //10 years
+        httpOnly: true,
+        sameSite: 'lax', //csrf
+        secure: __prod__ //cookie only works in https
+      },
+      saveUninitialized: false,
+      secret: 'asdoifjasiodfjasodfijasodifj',
+      resave: false,
     })
+  )
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false
+    }),
+    context: ({ req, res }) => ({
+      req,
+      res,
+      redis,
+      userLoader: createUserLoader(),
+      updootLoader: createUpdootLoader(),
+    })
+  });
+
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
+
+  app.listen(4000, () => {
+    console.log('🚀 Server started on localhost:4000')
+  })
 };
 
 main();
