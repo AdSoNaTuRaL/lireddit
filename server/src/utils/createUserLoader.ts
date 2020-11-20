7879e3ed2c23bc755ac9680adff5d8e5263f0dbc
adsonatural <adsonhenriquesilva@gmail.com>
2020-11-20 12:14:55 +0100

    Creating loaders (dataloaders) for updoot and user

---
+++ server/src/utils/createUserLoader.ts
@@ -0,0 +1,14 @@
+import DataLoader from "dataloader";
+import { User } from "../entities/User";
+
+export const createUserLoader = () =>
+  new DataLoader<number, User>(async (userIds) => {
+    const users = await User.findByIds(userIds as number[]);
+    const userIdToUser: Record<number, User> = {};
+    users.forEach(user => {
+      userIdToUser[user.id] = user;
+    });
+
+    const sortedUsers = userIds.map(userId => userIdToUser[userId]);
+    return sortedUsers;
+  });
