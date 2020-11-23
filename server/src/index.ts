3c36c777b3a480cea93e2f06bfa213f769966197
adsonatural <adsonhenriquesilva@gmail.com>
2020-11-23 12:23:35 +0100

    Chaging domain

--- server/src/index.ts
+++ server/src/index.ts
@@ -53,7 +53,7 @@ const main = async () => {
         httpOnly: true,
         sameSite: 'lax', //csrf
         secure: __prod__, //cookie only works in https
-        domain: __prod__ ? '.codeponder.com' : undefined,
+        domain: __prod__ ? 'adsonatural.online' : undefined,
       },
       saveUninitialized: false,
       secret: process.env.SESSION_SECRET as string,
